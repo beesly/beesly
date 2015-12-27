4374bbe5a59d2355ed41e26c10014a43c87c9bee
Kristopher Wilson <kristopherwilson@gmail.com>
2015-12-27 10:13:12 -0500

    BaseResource -> Resource

--- __tests__/resource/base-resource_test.js
+++ __tests__/resource/base-resource_test.js
@@ -1,9 +1,9 @@
-import BaseResource from '../../src/resource/base-resource';
+import Resource from '../../src/resource/resource';
 
-describe('BaseResource', () => {
+describe('Resource', () => {
   describe('hydrate()', () => {
     it('should hydrate the simple properties', () => {
-      var resource = new BaseResource();
+      var resource = new Resource();
       resource.hydrate({foo: 1, bar: 'none'});
 
       expect(resource.foo).toBe(1);
@@ -11,7 +11,7 @@ describe('BaseResource', () => {
     });
 
     it('should hydrate embedded resources', () => {
-      var resource = new BaseResource(),
+      var resource = new Resource(),
         embedded = {colors: [{name: 'red'}]};
       resource.hydrate({_embedded: embedded});
 
@@ -20,7 +20,7 @@ describe('BaseResource', () => {
     });
 
     it('should hydrate links', () => {
-      var resource = new BaseResource(),
+      var resource = new Resource(),
         links = {self: {href: 'me.com'}};
 
       resource.hydrate({_links: links});
@@ -32,7 +32,7 @@ describe('BaseResource', () => {
 
   describe('getLink()', () => {
     it('should retrieve the link by name', () => {
-      var resource = new BaseResource(),
+      var resource = new Resource(),
         links = {self: {href: 'me.com'}};
 
       resource.hydrate({_links: links});
@@ -41,14 +41,14 @@ describe('BaseResource', () => {
     });
 
     it('should return undefined if the link doesn\'t exist', () => {
-      var resource = new BaseResource({self: {href: 'me.com'}});
+      var resource = new Resource({self: {href: 'me.com'}});
       expect(resource.getLink('foobar')).toBeUndefined();
     });
   });
 
   describe('getEmbedded()', () => {
     it('should retrieve the resource by name', () => {
-      var resource = new BaseResource(),
+      var resource = new Resource(),
         embedded = {something: [{id: 100}]};
 
       resource.hydrate({_embedded: embedded});
@@ -57,28 +57,28 @@ describe('BaseResource', () => {
     });
 
     it('should return undefined if the resources does not exist', () => {
-      var resource = new BaseResource();
+      var resource = new Resource();
       expect(resource.getEmbedded('something')).toBeUndefined();
     });
 
     it('should return a single, hydrated resource when configured', () => {
-      var resource = new BaseResource();
-      resource.hasOne('home', BaseResource);
+      var resource = new Resource();
+      resource.hasOne('home', Resource);
       resource.hydrate({_embedded: {home: [{color: 'blue'}]}});
 
-      expect(resource.getEmbedded('home')).toEqual(jasmine.any(BaseResource));
+      expect(resource.getEmbedded('home')).toEqual(jasmine.any(Resource));
     });
 
     it('should return a collection of hydrated resources when configured', () => {
-      var resource = new BaseResource();
-      resource.hasMany('homies', BaseResource);
+      var resource = new Resource();
+      resource.hasMany('homies', Resource);
       resource.hydrate({_embedded: {homies: [{name: 'Clark'}, {name: 'Pineapple Face'}]}});
 
       expect(resource.getEmbedded('homies')).toEqual(jasmine.any(Array));
       expect(resource.getEmbedded('homies').length).toBe(2);
 
       resource.getEmbedded('homies').forEach((homie) => {
-        expect(homie).toEqual(jasmine.any(BaseResource));
+        expect(homie).toEqual(jasmine.any(Resource));
       });
 
     });
@@ -86,11 +86,11 @@ describe('BaseResource', () => {
 
   xdescribe('static get()', () => {
     pit('should return a hydrated instance', () => {
-      BaseResource.url = 'http://foo.com/{id}';
+      Resource.url = 'http://foo.com/{id}';
 
-      return BaseResource.get({id: 5}).then((resource) => {
+      return Resource.get({id: 5}).then((resource) => {
         expect(fetch.lastUrl).toEqual('http://foo.com/5');
-        expect(resource).toEqual(jasmine.any(BaseResource));
+        expect(resource).toEqual(jasmine.any(Resource));
         expect(resource.name).toBe('bob');
       });
     });
