445a3ba908b48b1c48d04d142cd9c4ed898357be
Kristopher Wilson <kristopherwilson@gmail.com>
2016-08-25 15:55:16 -0400

    Expose ResourceCollection

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('babel-register');
 
 module.exports = {
   Resource: require('./src/resource/resource').default,
+  ResourceCollection: require('./src/resource/resource-collection').default,
   Http: require('./src/http/http').default,
   Request: require('./src/http/request').default,
   Response: require('./src/http/response').default
